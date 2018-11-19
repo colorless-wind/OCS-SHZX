90f8da06ee3bc9a6da415ae7935699cb2496e90f
blue-eyes <e3156bios@163.com>
2018-11-19 18:34:56 +0800

    OCS 商户中心 初版

---
+++ login/scripts.js
@@ -0,0 +1,16 @@
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+    $.backstretch("assets/img/backgrounds/1.jpg");
+    
+    /*
+        Form validation
+    */
+    $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+});
